0ef964ef01e3a92801b66ffcddcde6c91a49fa56
Luis  Fernandez Jimenez <lfernand@gamma16.aulas.gsyc.urjc.es>
2016-10-17 10:13:22 +0200

    Acabado ejercicio 4

--- client.py
+++ client.py
@@ -8,9 +8,9 @@ import socket
 import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = sys.argv[1] # 'localhost'
-PORT = int(sys.argv[2]) # 6001
-LINE = ' '.join(sys.argv[3:]) # '¡Hola mundo!'
+SERVER = sys.argv[1]
+PORT = int(sys.argv[2]) 
+LINE = ' '.join(sys.argv[3:]) # pasar lista a string
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
