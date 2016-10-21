5474263590fb68cf2b0c55dc6b1706aa8b2d236f
Luis  Fernandez Jimenez <lfernand@alpha24.aulas.gsyc.urjc.es>
2016-10-21 12:53:13 +0200

    Ejercicio 10 acabado y con pep8 y pep257

--- client.py
+++ client.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente UDP que abre un socket a un servidor
-"""
+"""Programa cliente UDP que abre un socket a un servidor."""
 
 import socket
 import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2]) 
-line = ' '.join(sys.argv[3:]) # pasar lista a string
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    METODO = sys.argv[3]
+    LOGIN = sys.argv[4]
+    EXPIRES = int(sys.argv[5])
+except IndexError:
+    print('Usage: client.py ip puerto register sip_address expires_value')
 
-if line[:8] == 'register':
-	line = line[:8].upper() + ' sip:' + line[8:] + ' SIP/2.0\r\n\r\n'
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", line)
-    my_socket.send(bytes(line, 'utf-8') + b'\r\n')
+    Primer_Mensaje = (METODO.upper() + ' sip:' + LOGIN + ' SIP/2.0\r\n')
+    Segundo_Mensaje = ('Expires: ' + str(EXPIRES) + '\r\n')
+    print('Enviando: ' + Primer_Mensaje + Segundo_Mensaje)
+    my_socket.send(bytes(Primer_Mensaje, 'utf-8')
+                   + bytes(Segundo_Mensaje, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
-
+    print('Recibido --> ', data.decode('utf-8'))
 print("Socket terminado.")
